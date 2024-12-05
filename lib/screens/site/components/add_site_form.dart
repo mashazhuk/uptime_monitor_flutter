9ca9a4786e6d355afbded4d0cd35fd876adb683e
maria <mzhukova.zp@gmail.com>
2024-12-05 16:07:08 +0200

    added file add_site_form

---
+++ lib/screens/site/components/add_site_form.dart
@@ -0,0 +1,27 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// class AddSiteForm extends StatefulWidget {
+//   const AddSiteForm({super.key});
+//   @override
+//
+//   AddSiteFormState createState() {
+//     return AddSiteFormState();
+//   }
+// // State<AddSite> createState() => _AddSiteState();
+// }
+//
+// class AddSiteFormState extends State<AddSiteForm> {
+//   final _formKey = GlobalKey<FormState>();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // final AllSite siteInfo = ModalRoute.of(context)!.settings.arguments as AllSite;
+//     return Form(
+//         key: _formKey,
+//         child: const Column(
+//           children: [
+//             // TextFormField
+//           ]
+//         )
+//     )
